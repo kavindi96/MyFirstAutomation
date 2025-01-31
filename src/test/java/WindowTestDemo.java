625ee8a45669c7b1d32343054518343d69187b52
kavindi96 <Imalsha.kavindi96@gmail.com>
2025-01-31 14:52:10 +0530

    Update WindowTestDemo.java

--- src/test/java/WindowTestDemo.java
+++ src/test/java/WindowTestDemo.java
@@ -7,7 +7,8 @@ import java.util.Set;
 
 public class WindowTestDemo {
     public static void main(String[] args) throws InterruptedException {
-        newTabbedWindowTest();
+        //newTabbedWindowTest();
+        newSeparateWindowTest();
 
 
 
@@ -43,5 +44,43 @@ public class WindowTestDemo {
         System.out.println(driver.getTitle());
         driver.switchTo().window(parentWindow);
         //driver.switchTo().defaultContent();
+    }
+
+
+    public static void newSeparateWindowTest() throws InterruptedException{
+        WebDriver driver = WebDriverManager.chromiumdriver().create();
+        driver.manage().window().maximize();
+        //   Set<Cookie> cookies = driver.manage().getCookies();
+        //   Cookie jsessionid = driver.manage().getCookieNamed("JSESSIONID");
+        //   driver.manage().deleteCookie(jsessionid);
+        //   driver.manage().getCookieNamed("JSESSIONID");
+        driver.manage().deleteAllCookies();
+        driver.get("https://demo.automationtesting.in/Windows.html");
+
+        driver.findElement(By.xpath("//a[contains(text(),'Open New Seperate Windows')]")).click();
+
+        //Store the parent window
+        String parentWindow = driver.getWindowHandle();
+
+        driver.findElement(By.xpath("//button[text()='click']")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+
+        Set<String> childWindows = driver.getWindowHandles();
+
+        for (String window : childWindows){
+            if (window.equals(parentWindow)){
+                driver.switchTo().window(window);
+                break;
+            }
+        }
+
+        System.out.println(driver.getTitle());
+        driver.close();
+        Thread.sleep(3000);
+        driver.switchTo().window(parentWindow);
+        //driver.switchTo().defaultContent();
+        System.out.println(driver.getTitle());
+
     }
 }
